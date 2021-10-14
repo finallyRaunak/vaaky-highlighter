74f0789df049e619f998fa2f9bf3127c417d5749
Raunak Gupta <emperordevil.gupta@gmail.com>
2021-10-14 20:09:14 +0530

    Feature - Added support for YAML/YML.

--- Admin/js/block.js
+++ Admin/js/block.js
@@ -1,8 +1,8 @@
-const {__} = wp.i18n;
-const {RawHTML} = wp.element;
-const {registerBlockType} = wp.blocks;
-const {PanelBody, SelectControl, TextareaControl} = wp.components;
-const {InspectorControls, useBlockProps} = wp.blockEditor;
+const { __ } = wp.i18n;
+const { RawHTML } = wp.element;
+const { registerBlockType } = wp.blocks;
+const { PanelBody, SelectControl, TextareaControl } = wp.components;
+const { InspectorControls, useBlockProps } = wp.blockEditor;
 
 const blockStyle = {
     backgroundColor: '#d8d3d6',
@@ -10,41 +10,41 @@ const blockStyle = {
     padding: '20px'
 };
 const languages = [
-    {label: __('Select a language', 'vaaky-highlighter'), value: ''},
-    {label: 'Apache', value: 'apache'},
-    {label: 'Bash', value: 'bash'},
-    {label: 'C', value: 'c'},
-    {label: 'C#', value: 'csharp'},
-    {label: 'C++', value: 'cpp'},
-    {label: 'CSS', value: 'css'},
-    {label: 'DNS Zone file', value: 'dns'},
-    {label: 'DOS', value: 'cmd'},
-    {label: 'Django', value: 'django'},
-    {label: 'Dockerfile', value: 'docker'},
-    {label: 'Erlang', value: 'erlang'},
-    {label: 'Go', value: 'golang'},
-    {label: 'HTML', value: 'html'},
-    {label: 'XML', value: 'xml'},
-    {label: 'Handlebars', value: 'handlebars'},
-    {label: 'JSON', value: 'json'},
-    {label: 'Java', value: 'java'},
-    {label: 'JavaScript', value: 'javascript'},
-    {label: 'Markdown', value: 'markdown'},
-    {label: 'Nginx', value: 'nginx'},
-    {label: 'Objective C', value: 'objectivec'},
-    {label: 'PHP', value: 'php'},
-    {label: 'Plaintext', value: 'plaintext'},
-    {label: 'PostgreSQL & PL/pgSQL', value: 'pgsql'},
-    {label: 'PowerShell', value: 'powershell'},
-    {label: 'Python', value: 'python'},
-    {label: 'R', value: 'r'},
-    {label: 'Ruby', value: 'ruby'},
-    {label: 'Rust', value: 'rust'},
-    {label: 'SCSS', value: 'scss'},
-    {label: 'SQL', value: 'sql'},
-    {label: 'Shell', value: 'shell'},
-    {label: 'Twig', value: 'twig'},
-    {label: 'TypeScript', value: 'typescript'}
+    { label: __('Select a language', 'vaaky-highlighter'), value: '' },
+    { label: 'Apache', value: 'apache' },
+    { label: 'Bash', value: 'bash' },
+    { label: 'C', value: 'c' },
+    { label: 'C#', value: 'csharp' },
+    { label: 'C++', value: 'cpp' },
+    { label: 'CSS', value: 'css' },
+    { label: 'DNS Zone file', value: 'dns' },
+    { label: 'DOS', value: 'dos' },
+    { label: 'Django', value: 'django' },
+    { label: 'Dockerfile', value: 'dockerfile' },
+    { label: 'Go', value: 'golang' },
+    { label: 'HTML', value: 'html' },
+    { label: 'XML', value: 'xml' },
+    { label: 'Handlebars', value: 'handlebars' },
+    { label: 'JSON', value: 'json' },
+    { label: 'Java', value: 'java' },
+    { label: 'JavaScript', value: 'javascript' },
+    { label: 'Markdown', value: 'markdown' },
+    { label: 'Nginx', value: 'nginx' },
+    { label: 'Objective C', value: 'objectivec' },
+    { label: 'PHP', value: 'php' },
+    { label: 'Plaintext', value: 'plaintext' },
+    { label: 'PostgreSQL & PL/pgSQL', value: 'pgsql' },
+    { label: 'PowerShell', value: 'powershell' },
+    { label: 'Python', value: 'python' },
+    { label: 'R', value: 'r' },
+    { label: 'Ruby', value: 'ruby' },
+    { label: 'Rust', value: 'rust' },
+    { label: 'SCSS', value: 'scss' },
+    { label: 'SQL', value: 'sql' },
+    { label: 'Shell', value: 'shell' },
+    { label: 'Twig', value: 'twig' },
+    { label: 'TypeScript', value: 'typescript' },
+    { label: 'YAML/YML', value: 'yaml' }
 ];
 
 registerBlockType('vaaky-highlighter/code-khand', {
@@ -65,47 +65,47 @@ registerBlockType('vaaky-highlighter/code-khand', {
     keywords: [__('code', 'vaaky-highlighter'), __('syntax', 'vaaky-highlighter'), __('vaaky', 'vaaky-highlighter'), __('sourcecode', 'vaaky-highlighter'), __('format', 'vaaky-highlighter'), __('snippet', 'vaaky-highlighter')],
 
     edit: (props) => {
-        const blockProps = useBlockProps({style: blockStyle, className: 'vaaky-highlighter'});
+        const blockProps = useBlockProps({ style: blockStyle, className: 'vaaky-highlighter' });
         const {
-            attributes: {content, language},
+            attributes: { content, language },
             setAttributes,
             className
         } = props;
 
         return (
-                <div className={ className } { ...blockProps }>
-                    <InspectorControls>
-                        <PanelBody
-                            title={ __('Vaaky Highlighter Settings', 'vaaky-highlighter') }
-                            >
-                            <SelectControl
-                                label={ __('Language', 'vaaky-highlighter') }
-                                value={ language }
-                                options={ languages }
-                                onChange={ value => setAttributes({language: value}) }
-                                />
-                        </PanelBody>
-                    </InspectorControls>
-
-                    <TextareaControl
-                        label="Code Snippet"
-                        value={ content }
-                        onChange={ value => setAttributes({content: value}) }
-                        rows="10"
+            <div className={className} {...blockProps}>
+                <InspectorControls>
+                    <PanelBody
+                        title={__('Vaaky Highlighter Settings', 'vaaky-highlighter')}
+                    >
+                        <SelectControl
+                            label={__('Language', 'vaaky-highlighter')}
+                            value={language}
+                            options={languages}
+                            onChange={value => setAttributes({ language: value })}
                         />
-                </div>
-                );
+                    </PanelBody>
+                </InspectorControls>
+
+                <TextareaControl
+                    label="Code Snippet"
+                    value={content}
+                    onChange={value => setAttributes({ content: value })}
+                    rows="10"
+                />
+            </div>
+        );
     },
     save: (props) => {
         const {
-            attributes: {content, language}
+            attributes: { content, language }
         } = props;
         var myShortcode = '[vaakyHighlighterCode lang="' + language + '"]' + htmlEntities(content) + '[/vaakyHighlighterCode]';
         return (
-                <div>
-                    <RawHTML>{ myShortcode }</RawHTML>
-                </div>
-                );
+            <div>
+                <RawHTML>{myShortcode}</RawHTML>
+            </div>
+        );
     }
 })
 
