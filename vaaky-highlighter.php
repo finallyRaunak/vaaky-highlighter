cd221a0598eda2a6aa8fed6cc1d43962a4964c4e
Raunak Gupta <emperordevil.gupta@gmail.com>
2022-03-20 23:15:22 +0530

    Quickfix/webshite URL (#2)
    * Tweak - website URL & update wp version.
    * Dev - tweak typo.

--- vaaky-highlighter.php
+++ vaaky-highlighter.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Vaaky Highlighter
  * Plugin URI:        https://wordpress.org/plugin/vaaky-highlighter
  * Description:       Simple yet elegant syntax or code highlighter based on highlight.js. It allows you to add engaging snippet code blocks.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Raunak Gupta
  * Author URI:        https://www.webhat.in/
  * License:           GPL-2.0+
@@ -39,7 +39,7 @@ require_once plugin_dir_path(__FILE__) . 'Autoloader.php';
 /**
  * Current plugin version.
  */
-define('VAAKY_HIGHLIGHTER_VERSION', '1.0.1');
+define('VAAKY_HIGHLIGHTER_VERSION', '1.0.2');
 
 /**
  * The string used to uniquely identify this plugin.
